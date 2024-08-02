2bb3bee5b95e8025dfd069cd284d93bf073f9294
IrrelevantIdiot <naomivdtoorn@hotmail.com>
2024-08-02 13:05:15 +0200

    Add files via upload

---
+++ Barbara Kempers Unlimited.js
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body><table width="1200" border="0" cellspacing="0" cellpadding="0">
+  <tbody>
+    <tr>
+      <td><img src="../OneDrive/Bureaublad/Barbara Kempers Unlimited/Images/Barbara Kempers Unlimited.png" width="775" height="55" alt=""/></td>
+    </tr>
+  </tbody><table width="1200" border="0" cellspacing="0" cellpadding="0">
+  <tbody>
+    <tr>
+      <td><img src="../OneDrive/Bureaublad/Barbara Kempers Unlimited/Images/Home button.png" width="83" height="35" alt=""/><img src="../OneDrive/Bureaublad/Barbara Kempers Unlimited/Images/Services.png" width="84.5" height="35" alt=""/><img src="../OneDrive/Bureaublad/Barbara Kempers Unlimited/Images/Company.png" width="84.5" height="35" alt=""/><img src="../OneDrive/Bureaublad/Barbara Kempers Unlimited/Images/Contact.png" width="84" height="35" alt=""/></td>
+      <td><img src="../OneDrive/Bureaublad/Barbara Kempers Unlimited/Images/White space.png" width="141.5" height="47" alt=""/></td>
+      <td><img src="../OneDrive/Bureaublad/Barbara Kempers Unlimited/Images/Linked in.png" width="59" height="54.5" alt=""/><img src="../OneDrive/Bureaublad/Barbara Kempers Unlimited/Images/Instagram.png" width="56" height="55.5" alt=""/></td>
+    </tr>
+  </tbody>
+</table>
+
+</table>
+
+</body><table width="1200" border="0" cellspacing="0" cellpadding="0">
+  <tbody>
+    <tr>
+      <td><img src="../OneDrive/Bureaublad/Barbara Kempers Unlimited/Images/Slogan.png" width="593" height="69" alt=""/></td>
+    </tr>
+  </tbody>
+</table>
+
+</html>
